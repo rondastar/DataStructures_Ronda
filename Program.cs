7274658614389e3511366cdc2b0c6abf598bccfe
rondastar <116147793+rondastar@users.noreply.github.com>
2024-02-10 09:24:07 -0800

    Added and tested stack class

--- Program.cs
+++ Program.cs
@@ -7,8 +7,97 @@
     {
         static void Main(string[] args)
         {
-            QueueTest();
+            StackTest();
+            //QueueTest(); 
         } // Main
+
+        static void StackTest()
+        {
+            // ====== Test stack with string ===========================
+            Console.WriteLine("Test stack with string:");
+            // create a new stack
+            Stack<string> stack = new Stack<string>();
+
+            // test push
+            stack.Push("apricot");
+            stack.Push("banana");
+            stack.Push("coconut");
+            stack.Push("durian");
+            stack.Push("eggplant");
+
+            // test count
+            Console.WriteLine(stack.Count); // 5
+
+            // test pop
+            stack.Pop();
+
+            // test peek
+            Console.WriteLine(stack.Peek()); // durian
+
+            // test clear
+            stack.Clear();
+
+            // test count
+            Console.WriteLine(stack.Count); // 0
+
+            // test peek
+            Console.WriteLine(stack.Peek()); // blank
+
+            // test pop
+            Console.WriteLine(stack.Pop()); // blank
+
+            // test push
+            stack.Push("apple");
+
+            // test pop
+            Console.WriteLine(stack.Pop()); // apple
+
+            // test peek
+            Console.WriteLine(stack.Peek()); // blank
+
+            // ====== Test stack with float ===========================
+            Console.WriteLine("Test stack with float:");
+            // create a new stack
+            Stack<float> stackFloat = new Stack<float>();
+
+            // test push
+            stackFloat.Push(1.2F);
+            stackFloat.Push(2.3F);
+            stackFloat.Push(3.4F);
+            stackFloat.Push(4.4F);
+            stackFloat.Push(5.6F);
+
+            // test count
+            Console.WriteLine(stackFloat.Count); // 5
+
+            // test pop
+            stackFloat.Pop();
+
+            // test peek
+            Console.WriteLine(stackFloat.Peek()); // 4.4
+
+            // test clear
+            stackFloat.Clear();
+
+            // test count
+            Console.WriteLine(stackFloat.Count); // 0
+
+            // test peek
+            Console.WriteLine(stackFloat.Peek()); // 0
+
+            // test pop
+            Console.WriteLine(stackFloat.Pop()); // 0
+
+            // test push
+            stackFloat.Push(7.5F);
+
+            // test pop
+            Console.WriteLine(stackFloat.Pop()); // 7.5
+
+            // test peek
+            Console.WriteLine(stackFloat.Peek()); // 0
+
+        } // StackTest
 
         static void QueueTest()
         {
@@ -78,5 +167,5 @@
             // test count
             Console.WriteLine(intQueue.Count); // 0
         } // QueueTest
-    }
-}
+    } // class
+} // namespace
--- Stack.cs
+++ Stack.cs
@@ -6,7 +6,115 @@ using System.Threading.Tasks;
 
 namespace DataStructures_Ronda
 {
-    internal class Stack
+    internal class Stack<T>
     {
-    }
-}
+        internal StackNode<T> top;   // top element in the stack
+        internal int count;          // tracks the number of elements in the stack
+
+        public Stack()
+        {
+            top = null;     
+            count = 0;
+        } // Stack constructor for empty stack
+
+        internal class StackNode<T> // nested class for the nodes of the stack
+        {
+            internal T Value { get; set; }            // Data stored in the node
+            internal StackNode<T> Next { get; set; }    // Reference to the next node
+
+            internal StackNode(T value)   // creates a node with the given value
+            {
+                Value = value;
+                Next = null;
+            } // StackNode constructor
+
+        } // StackNode nested class
+
+        // Count provides access to the number of elements in the stack
+        public int Count { get => count; set => count = value; }
+
+        // Push adds an element to the top
+        public void Push(T value)
+        {
+            // Create a new node with the input value
+            StackNode<T> newNode = new StackNode<T>(value);
+
+            if (newNode != null)
+            {
+                // Create a temporary node to keep track of the current top node
+                StackNode<T> current = top;
+
+                // Update the references of the new node so that the current top follows the new node
+                newNode.Next = current;
+            }
+
+            // Assign the new node to be the top
+            top = newNode;
+
+            // increment count to reflect added element
+            count++;
+
+        } // Push
+
+        // Pop removes and returns the element from the top
+        public T Pop()
+        {
+            // If top is null, return default
+            if (top == null)
+            {
+                return default(T);
+            }
+
+            // Assign value to be removed from top to a temporary variable
+            T temp = top.Value;
+
+            // If the next node is empty, reset top to null
+            if (top.Next == null) top = null;
+
+            // If the next node is not null, update reference so it is the new top
+            else if (top.Next != null)
+            {
+                top = top.Next;
+            }
+
+            // decrement the count
+            count--;
+
+            // return the value that was removed
+            return temp;
+        } // Pop
+
+        // Peek returns the value of the top without removing it
+        public T Peek()
+        {
+            // If top is null, return default
+            if (top == null)
+            {
+                return default(T);
+            }
+
+            // return the value of the top
+            return top.Value;
+        } // Peek
+
+        public void Clear()
+        {
+            // runs until the stack is empty
+            while (top != null)
+            {
+                // If the next node is empty, reset top to null
+                if (top.Next == null) top = null;
+
+                // If the next node is not null, update reference so it is the new top
+                else if (top.Next != null)
+                {
+                    top = top.Next;
+                }
+
+                // decrement the count
+                count--;
+            }
+        } // Clear
+
+    } // Stack class
+} // namespace
